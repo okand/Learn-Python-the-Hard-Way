5b25b924a1c64804e4bd045bb22289eba673e321
Alexander Rennerfelt <alexander@rennerfelt.org>
2015-05-16 02:50:23 +0200

    Integrated metric numbers

--- ex5.py
+++ ex5.py
@@ -5,10 +5,14 @@ weight = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+inch_in_cm = 2.54
+lbs_in_kg = 0.45359237
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %d inches (or %d centimeters) tall." % (
+    height, height * inch_in_cm)
+print "He's %d pounds (or %d kilograms) heavy." % (
+    weight, weight * lbs_in_kg)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
@@ -17,9 +21,3 @@ print "His teeth are usually %s depending on the coffee." % teeth
 print "if I add %d, %d and %d I get %d." % (
     age, height, weight, age + height + weight)
 
-# this part converts the numbers to metric
-inch_in_cm = 2.54
-lbs_in_kg = 0.45359237
-
-print "Weight in kilograms:", weight * lbs_in_kg
-print "Height in centimeters:", height * inch_in_cm
